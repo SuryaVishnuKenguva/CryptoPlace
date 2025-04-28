3a28c8ad9b34e873616653c0dde0eb15e988f345
Surya Vishnu <suryakenguva143@gmail.com>
2025-04-28 19:43:43 +0530

    Added Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import CoinContextProvider from './context/CoinContext.jsx'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <CoinContextProvider>
          <App />
+         <SpeedInsights />
       </CoinContextProvider>
     </BrowserRouter>
   </StrictMode>,
